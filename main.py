5b8fc019aae37b60b9cb9e00f932eee21d6f36cb
James Coll <collj@bu.edu>
2022-10-28 10:26:10 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from controllers.players_controller import players_controller_bp
+
+# create application object and register blueprints
+app = Flask(__name__)
+app.register_blueprint(players_controller_bp)
+
+# Run app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/player.py
@@ -0,0 +1,8 @@
+class Player:
+    player_id = 1 # unique id
+    position_id = "QB" # may need to use lookup table for position
+    description = "desc123" # desc for about page
+    stats = "test" # add stats object, dependent on position
+
+
+
