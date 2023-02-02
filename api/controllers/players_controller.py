dab2fc01a8aa8c3f33cbce1280e71d523b26d152
James Coll <collj@bu.edu>
2023-02-02 13:36:47 -0500

    update sql alchemy and querying

--- api/controllers/players_controller.py
+++ api/controllers/players_controller.py
@@ -1,7 +1,10 @@
 import os
 
 from flask import Blueprint
+from sqlalchemy import select
+
 from api.models.player import db, Player
+from api.models.player_stats import PlayerStats
 from api.schemas.PlayerSchema import PlayerSchema
 from api.schemas.PlayerStatsSchema import PlayerStatsSchema
 from utils.s3_utils import create_presigned_url
@@ -13,16 +16,25 @@ player_stats_schema = PlayerStatsSchema()
 
 
 # fetch all players
-@players_controller_bp.route('/players', methods=['GET'])
+@players_controller_bp.route('/GetPlayers', methods=['GET'])
 def get_players():
-    result_list = db.session.query(Player)
-    return player_schema.dumps(result_list, many=True)
+    stmt = select(Player)
+    players = db.session.scalars(stmt).all()
+
+    return player_schema.dumps(players, many=True)
 
 
 # fetch players by id
-@players_controller_bp.route('/players/<player_id>', methods=['GET'])
+@players_controller_bp.route('/GetPlayerById/<int:player_id>', methods=['GET'])
 def get_player_by_id(player_id):
     player_info = db.get_or_404(Player, player_id)
 
     return player_schema.dumps(player_info)
 
+
+@players_controller_bp.route('/GetPlayerStats/<int:player_id>', methods=['GET'])
+def get_player_stats(player_id):
+    stmt = select(PlayerStats).where(PlayerStats.player_id == player_id)
+    stats = db.session.scalars(stmt).all()
+
+    return player_stats_schema.dumps(stats, many=True)
