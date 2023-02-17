308e1cbd3248ab8ffa728b1b780919bef0d41885
James Coll <collj@bu.edu>
2023-02-17 13:28:31 -0500

    flask migrate support

--- utils/position_enum.py
+++ utils/position_enum.py
@@ -4,3 +4,20 @@ from enum import Enum
 class PositionEnum(Enum):
     QB = 1
     WR = 2
+    RB = 3
+    LT = 4
+    LG = 5
+    C = 6
+    RG = 7
+    RT = 8
+    TE = 9
+    DT = 10
+    DE = 11
+    OLB = 12
+    MLB = 13
+    CB = 14
+    SS = 15
+    FS = 16
+
+
+
